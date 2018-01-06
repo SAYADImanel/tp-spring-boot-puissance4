89696ba57b3c4d493c6dce782aaeea971a4b5722
LAPTOP-97H7B5O5\manel <manel.sayadi@yahoo.fr>
2018-01-06 18:32:53 +0100

    affichage du tableau de jeu

--- src/main/java/com/igs/ipi/tpspringbootsayadi/model/GameModel.java
+++ src/main/java/com/igs/ipi/tpspringbootsayadi/model/GameModel.java
@@ -2,9 +2,23 @@ package com.igs.ipi.tpspringbootsayadi.model;
 
 public class GameModel {
 
+    private static final int l = 7;
+
+    private static final int h = 6;
+
     private String  nom1;
 
     private String nom2;
+
+    private Jeton [][] tableau = new Jeton [h] [l];
+
+    public GameModel() {
+             for (int i = 0; i < tableau.length; i++) {
+                 for (int j = 0; j < tableau[i].length; j++) { tableau[i][j] = Jeton.VIDE; }
+                  }
+          }
+
+
 
     public String getNom1() {
         return nom1;
@@ -14,8 +28,6 @@ public class GameModel {
         this.nom1 = nom1;
     }
 
-
-
     public String getNom2() {
         return nom2;
     }
@@ -23,4 +35,13 @@ public class GameModel {
     public void setNom2(String nom2) {
         this.nom2 = nom2;
     }
+
+           public Jeton[][] gettableau() {
+             return tableau;
+    }
+
+             public void settableau(Jeton[][] tableau) {
+               this.tableau = tableau;
+    }
+
 }
---
+++ src/main/java/com/igs/ipi/tpspringbootsayadi/model/Jeton.java
@@ -0,0 +1,5 @@
+package com.igs.ipi.tpspringbootsayadi.model;
+
+public enum Jeton {
+   ROUGE, JAUNE, VIDE
+ }
